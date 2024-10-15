03f93bf8e5ace061b6eb28dfbf784f2561a97d34
Sanko <sanko.eko@gmail.com>
2024-10-15 20:35:27 +0500

    user relation comment

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,6 +42,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+    //модель отвечающий за назначение тайной санты
     public function santaRelation()
     {
         return $this->hasOne(UserSecretSanta::class,'id');
